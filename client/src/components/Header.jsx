c36b8872742eb6da951b68ccd7803892a9f6e00e
JUDHI3134 <judhistirbehera456@gmail.com>
2025-06-23 15:42:29 +0530

    connect frontend with backend with add blog, fetch all blog

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { assets } from '../assets/assets'
+import { useAppContext } from '../context/AppContext'
 
 const Header = () => {
+
+  const { input, setInput } = useAppContext()
+  const inputRef = useRef()
+
+  const onSubmitHandler = async(e) => {
+    e.preventDefault();
+    setInput(inputRef.current.value)
+  }
+
+  const onClear = () => {
+    setInput('')
+    inputRef.current.value = ''
+  }
+
   return (
     <div className='mx-8 sm:mx-16 xl:mx-24 relative'>
           <div className='text-center mt-20 mb-8'>
@@ -15,12 +30,17 @@ const Header = () => {
 
               <p className='my-6 sm:my-8 max-w-2xl m-auto max-sm:text-xs text-gray-500'>Explore our AI-powered blog for smart insights, automated content, and real-time updates that transform writing, trends, and productivity using intelligent tools.</p>
 
-              <form className='flex justify-between max-w-lg max-sm:scale-75 mx-auto border border-gray-300 bg-white rounded overflow-hidden'>
-                  <input className='w-full pl-4 outline-none' type="text" placeholder='Search for blogs' required />
+              <form onSubmit={onSubmitHandler} className='flex justify-between max-w-lg max-sm:scale-75 mx-auto border border-gray-300 bg-white rounded overflow-hidden'>
+                  <input ref={inputRef} className='w-full pl-4 outline-none' type="text" placeholder='Search for blogs' required />
                   <button className=' bg-primary text-white px-8 py-2 m-1.5 rounded hover:scale-105 transition-all cursor-pointer' type="submit">Search</button>
               </form>
 
-          </div>
+      </div>
+      
+      <div className='text-center'>
+       {input && <button onClick={onClear} className='border font-light text-xs py-1 px-3 rounded-sm shadow-custom-sm cursor-pointer'>Clear Search</button>}
+      </div>
+
           <img src={assets.gradientBackground} alt="" className='absolute -top-50 -z-1 opacity-50' />
     </div>
   )
